cf69b94a54820fbee081ee1ed78ce16750bec358
Quinten De Vreese <69604348+qdevreese@users.noreply.github.com>
2020-08-13 21:25:08 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,15 +1,28 @@
 import discord
 from discord.ext import commands
 
+TOKEN = 'NzQyOTc2OTExNTc5MjgzNDg2.XzN9Kw.GlECebbNj7x-saIqjuTdvouLOdc'
 client = commands.Bot(command_prefix=".")
 
 @client.event
 async def on_ready():
     print("BOT IS WORKING CORRECTLY")
-    await client.change_presence(activity=discord.Game(name="Fucking shit up big time"))
+    await client.change_presence(activity=discord.Streaming(name="YOUR MOM", url = "https://www.youtube.com/watch?v=oHg5SJYRHA0"))
 
-@client.command()
-async def hello(ctx):
-    await ctx.send("Hi")
+#to set status to "Playing" remove "Streaming" and replace it with "Game" and remove the url.
 
-client.run('NzQyOTc2OTExNTc5MjgzNDg2.XzN9Kw.J7hTB8NBROLqW5JDRRVWkKP6UWo')
+@client.event
+async def on_member_join(member):
+    channel = discord. utils.get(member.guild.channels, name='xd')
+    await channel.send(f'{member.mention} #1 Who the fuck are you? #2 Why the fuck are you here?')
+
+#Discord member join message
+
+@client.event
+async def on_member_remove(member):
+    channel = discord.utils.get(member.guild.channels, name='xd')
+    await channel.send(f'{member.mention} we dont need your bitch ass anyways.')
+
+#Dsicord member leave message
+
+client.run(TOKEN)
