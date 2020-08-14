c688b957c40e95460ac354c1634134f0e9861ba0
popkdodge <popkdodge@gmail.com>
2020-08-13 22:49:04 -0400

    added command 8ball

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+from random import randint
 from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv()
@@ -7,25 +8,57 @@ load_dotenv()
 token = os.getenv('TOKEN')
 client = commands.Bot(command_prefix=".")
 
+
 @client.event
 async def on_ready():
+# to set status to "Playing" remove "Streaming" and replace it with "Game" and remove the url.
     print("BOT IS WORKING CORRECTLY")
-    await client.change_presence(activity=discord.Streaming(name="YOUR MOM", url = "https://www.youtube.com/watch?v=oHg5SJYRHA0"))
+    await client.change_presence(activity=discord.Streaming(name="YOUR MOM", url="https://www.youtube.com/watch?v=oHg5SJYRHA0"))
 
-#to set status to "Playing" remove "Streaming" and replace it with "Game" and remove the url.
+
 
 @client.event
+# Discord member join message
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.channels, name='xd')
     await channel.send(f'{member.mention} #1 Who the fuck are you? #2 Why the fuck are you here?')
 
-#Discord member join message
+
 
 @client.event
 async def on_member_remove(member):
+    # Discord member leave message
     channel = discord.utils.get(member.guild.channels, name='xd')
     await channel.send(f'{member.mention} we dont need your bitch ass anyways.')
 
-#Dsicord member leave message
+@client.command()
+async def _8ball(insert):
+    """8 Ball Command!"""
+    ball = [
+        'As I see it, yes.',
+        'Ask again later.',
+        'Better not tell you now.',
+        'Cannot predict now.',
+        'Concentrate and ask again.',
+        'Don’t count on it.',
+        'It is certain.',
+        'It is decidedly so.',
+        'Most likely.',
+        'My reply is no.',
+        'My sources say no.',
+        'Outlook not so good.',
+        'Outlook good.',
+        'Reply hazy, try again.',
+        'Signs point to yes.',
+        'Very doubtful.',
+        'Without a doubt.',
+        'Yes.',
+        'Yes – definitely.',
+        'You may rely on it.',
+        'Possibly.',
+        'No.'
+        ]
+    await insert.send(f'{ball[randint(0,23)]}')
+
 
 client.run(token)
